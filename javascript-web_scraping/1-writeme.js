80438548789640d4774aeaa393e0688ed8e8ee1c
LeoRod17 <matiassotelo171097@gmail.com>
2023-09-19 14:45:17 -0700

    task 1 standar fix

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,8 +3,8 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], error => {
-    if (error) {
-        console.error(error);
-        return;
-    }
+  if (error) {
+    console.error(error);
+    return;
+  }
 });
