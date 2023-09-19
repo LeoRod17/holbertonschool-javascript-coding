16d58028dd149844b13499cac3b93f5af6b9c9e2
LeoRod17 <matiassotelo171097@gmail.com>
2023-09-19 14:37:29 -0700

    task 0 standar fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-const fs = require('fs')
-
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (error, content) => {
   if (error) {
     console.error(error);
     return;
   }
-  console.log(content)
+  console.log(content);
 });
