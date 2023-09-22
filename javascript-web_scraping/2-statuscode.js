e0669b34cbeda350ab12b232c2949dd756d1aa7f
LeoRod17 <matiassotelo171097@gmail.com>
2023-09-22 12:25:56 -0700

    task 2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const req = require('request');
+
+req(process.argv[2], (error, content) => {
+    if (error) {
+        console.error(error);
+    }
+    console.log('code:', content.statusCode)
+});
